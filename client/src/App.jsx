7fadecfbbaeec3f8a379c0da5abc8ae3ec6a6892
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-07 10:27:59 +0700

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,32 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { DashboardLayout, HomeLayout, Login, Register } from './pages'
+import { DashboardLayout, Error, HomeLayout, Landing, Login, Register } from './pages'
 
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomeLayout />
-  },
-  {
-    path: '/register',
-    element: <Register />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/dashboard',
-    element: <DashboardLayout />
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/dashboard',
+        element: <DashboardLayout />
+      }
+    ]
   }
+
 ])
 
 function App() {
--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
 
 const Error = () => {
+    const error = useRouteError()
     return (
-        <div>Error</div>
+        <div>
+            <h1>Error page!</h1>
+            <Link to='/dashboard'>back home</Link>
+        </div>
     )
 }
 
