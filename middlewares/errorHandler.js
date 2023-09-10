1c142ecb2e44abe54f550a762eabff2c8548bf78
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-10 16:41:57 +0700

    created custom Error classes

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,5 +3,5 @@ export const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500
     const msg = err.message || 'Internal Server Error'
 
-    res.status(500).json({ msg })
+    res.status(statusCode).json({ msg })
 }
