25fea800449995b265dc74795f94119757829129
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-14 15:37:14 +0700

    created logout controller & route

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,3 +35,12 @@ export const login = async (req, res) => {
 
     res.status(200).json({ msg: 'user logged in' })
 }
+
+export const logout = (req, res) => {
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now())
+    })
+
+    res.status(200).json({ msg: 'user logged out' })
+}
