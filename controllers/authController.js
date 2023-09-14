1dda7e6105136e19fbe2261c8cda28e41eda23cf
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-14 11:08:12 +0700

    created jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,19 @@
 import { UnauthenticatedError } from "../errors/customError.js"
 import User from "../models/userModel.js"
 import { comparePassword, hashPassword } from "../utils/passwordUtils.js"
+import { createJwt } from "../utils/tokenUtils.js"
 
 
 
 export const register = async (req, res) => {
+    const isFirstAccount = (await User.countDocuments()) === 0
+    req.body.role = isFirstAccount ? 'admin' : 'user'
+
     const hashedPassword = await hashPassword(req.body.password)
     req.body.password = hashedPassword
 
     const user = await User.create(req.body)
-    res.status(201).json({ user })
+    res.status(201).json({ msg: 'user created' })
 }
 
 export const login = async (req, res) => {
@@ -19,4 +23,15 @@ export const login = async (req, res) => {
 
     const isValidUser = user && (await comparePassword(req.body.password, user.password))
     if (!isValidUser) throw new UnauthenticatedError('invalid credentials')
+
+    // generate token
+    const token = createJwt({ userId: user._id, role: user.role })
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        secure: process.env.NODE_ENV === 'production'
+    })
+
+    res.status(200).json({ msg: 'user logged in' })
 }
