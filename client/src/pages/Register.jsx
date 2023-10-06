0011bb970baf8a4c68a5d757ff3c5a594a88545b
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-06 15:44:07 +0700

    added action register route

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, Form, redirect, useNavigation } from 'react-router-dom'
 import { FormRow, Logo } from '../components'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { customFetch } from '../utils/customFetch'
+import { toast } from 'react-toastify'
+export const action = async ({ request }) => {
+    const formData = await request.formDate()
+    const data = Object.fromEntries(formData)
+    try {
+        await customFetch.post('/auth/register', data)
+        toast.success('Registration successful')
+        return redirect('/login')
+    } catch (error) {
+        toast.error(error?.response?.data?.msg)
+        return error
+    }
+}
+
 const Register = () => {
+    const navigation = useNavigation()
+    const isSubmitting = navigation.state === 'submitting'
     return (
         <Wrapper>
-            <form className='form'>
+            <Form method='post' className='form'>
                 <Logo />
                 <h4>Register</h4>
                 <FormRow type='text' name='name' />
@@ -14,11 +31,11 @@ const Register = () => {
 
                 <FormRow type='password' name='password' />
 
-                <button type='submit' className='btn btn-block'>
-                    submit
+                <button type='submit' className='btn btn-block' disabled={isSubmitting}>
+                    {isSubmitting ? 'submitting...' : 'submit'}
                 </button>
                 <p>Already a member? <Link to='/login' className='member-btn'>Login</Link></p>
-            </form>
+            </Form>
         </Wrapper>
     )
 }
