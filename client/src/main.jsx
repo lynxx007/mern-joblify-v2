8eb5b26b97a74edd5c5451fcca6ad37558255bfc
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-06 15:45:16 +0700

    added toast to root project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
-
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer position='top-center' />
     </Provider>
   </React.StrictMode>,
 )
