d1a6627db8b57a1eb10dc95cbb3271703e0a5f9a
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-14 09:25:03 +0700

    fixed typo

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import Job from "../models/jobModel.js"
 import User from "../models/userModel.js"
 
 export const getCurrentUser = async (req, res) => {
-    const user = await User.findOne({ id: req.user.userId })
+    const user = await User.findOne({ _id: req.user.userId })
     const userWithoutPassword = user.toJSON()
     res.status(200).json({ user: userWithoutPassword })
 }
