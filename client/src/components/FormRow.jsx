56143193468a13417f8f42be3c9a3b90c33a3121
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-19 16:53:07 +0700

    removed values prop

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,9 +1,9 @@
 
-const FormRow = ({ type, name, labelText, defaultValue = '', value, onChange }) => {
+const FormRow = ({ type, name, labelText, defaultValue = '', onChange }) => {
     return (
         <div className='form-row'>
             <label htmlFor={name} className='form-label'>{labelText || name}</label>
-            <input type={type} id={name} name={name} className='form-input' defaultValue={defaultValue} value={value} onChange={onChange} required />
+            <input type={type} id={name} name={name} className='form-input' defaultValue={defaultValue} onChange={onChange} required autoComplete="on" />
         </div>
     )
 }
