b4d7a587a356de3ac72127e772fdc601ec3c9c3d
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-21 12:23:44 +0700

    created Admin page

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa"
+import Wrapper from '../assets/wrappers/StatsContainer'
+import { StatItem } from "../components"
+import { useGetAppStatsQuery } from "../features/api/apiSlice"
 const Admin = () => {
-    return (
-        <div>Admin</div>
-    )
+    const { data, isSuccess } = useGetAppStatsQuery()
+
+    if (isSuccess)
+        return (
+            <Wrapper>
+                <StatItem
+                    title='current users'
+                    count={data?.users}
+                    color='#e9b949'
+                    bcg='#fcefc7'
+                    icon={<FaSuitcaseRolling />}
+                />
+                <StatItem
+                    title='total jobs'
+                    count={data?.jobs}
+                    color='#647acb'
+                    bcg='#e0e8f9'
+                    icon={<FaCalendarCheck />}
+                />
+            </Wrapper>
+        )
 }
 
 export default Admin
