cd7c726596769bce8d3de51ea6aae462feedffec
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-21 12:24:40 +0700

    added tags,delete getApp queries

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -5,13 +5,15 @@ export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: '/api/v1'
     }),
+    tagTypes: ['Jobs', 'Users'],
     endpoints: builder => ({
         register: builder.mutation({
             query: data => ({
                 url: '/auth/register',
                 method: 'POST',
                 body: data
-            })
+            }),
+            invalidatesTags: ['Users']
         }),
         login: builder.mutation({
             query: data => ({
@@ -37,26 +39,44 @@ export const apiSlice = createApi({
                 url: '/jobs',
                 method: 'POST',
                 body: data,
-            })
+            }),
+            invalidatesTags: ['Jobs']
         }),
         getJobs: builder.query({
             query: () => ({
                 url: '/jobs',
                 method: 'GET'
-            })
+            }),
+            providesTags: ['Jobs']
         }),
         getJob: builder.query({
             query: (id) => ({
                 url: `/jobs/${id}`,
                 method: 'GET'
-            })
+            }),
+            providesTags: ['Jobs']
         }),
         editJob: builder.mutation({
             query: ({ id, ...patch }) => ({
                 url: `/jobs/${id}`,
                 method: 'PATCH',
                 body: patch
-            })
+            }),
+            invalidatesTags: ['Jobs']
+        }),
+        deleteJob: builder.mutation({
+            query: (id) => ({
+                url: `/jobs/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Jobs']
+        }),
+        getAppStats: builder.query({
+            query: () => ({
+                url: '/users/admin/app-stats',
+                method: 'GET'
+            }),
+            providesTags: ['Users', 'Jobs']
         })
     })
 })
@@ -68,5 +88,7 @@ export const { useRegisterMutation,
     useAddJobMutation,
     useGetJobsQuery,
     useEditJobMutation,
-    useGetJobQuery } = apiSlice
+    useGetJobQuery,
+    useDeleteJobMutation,
+    useGetAppStatsQuery } = apiSlice
 
