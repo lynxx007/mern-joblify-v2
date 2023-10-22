9c76fa7d52ccfd5116beee1101cb76e85631ed61
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-22 14:22:22 +0700

    created showStats controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import { NotFoundError } from '../errors/customError.js'
 import Job from '../models/jobModel.js'
 export const createJob = async (req, res) => {
@@ -49,3 +50,66 @@ export const deleteJob = async (req, res) => {
     res.status(200).json({ job: removedJob })
 }
 
+export const showStats = async (req, res) => {
+    let stats = await Job.aggregate([
+        {
+            $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) }
+        },
+        {
+            $group: {
+                _id: '$jobStatus',
+                count: {
+                    $sum: 1
+                }
+
+            }
+        },
+    ])
+
+    stats = stats.reduce((acc, curr) => {
+        const { _id: jobStatus, count } = curr
+        acc[jobStatus] = count
+        return acc
+    }, {})
+
+    const defaultStats = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        declined: stats.declined || 0
+    }
+
+    let monthlyStats = await Job.aggregate([
+        {
+            $match: {
+                createdBy: new mongoose.Types.ObjectId(req.user.userId)
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    year: { $year: '$createdAt' },
+                    month: { $month: '$createdAt' }
+                },
+                count: { $sum: 1 }
+            }
+        },
+        {
+            $sort: {
+                '_id.year': -1,
+                '_id.month': -1
+            }
+        },
+        {
+            $limit: 6
+        }
+    ])
+
+    monthlyStats = monthlyStats.map(item => {
+        const { _id: { year, month }, count } = item
+
+        const date = day().month(month - 1).year(year).format('MMM YY')
+        return { date, count }
+    }).reverse()
+
+    res.status(200).json({ defaultStats, monthlyStats })
+}
