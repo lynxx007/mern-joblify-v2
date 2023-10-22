a3feef959950188c33e36c3ffaccc020732c904a
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-22 10:45:31 +0700

    added route for multer

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { validateUpdateUserInput } from "../middlewares/validations.js";
 import { authorizePermissions } from "../middlewares/authMiddleware.js";
+import upload from "../middlewares/multer.js";
 
 const router = Router();
 
 router.get('/current-user', getCurrentUser)
 router.get('/admin/app-stats', authorizePermissions('admin'), getApplicationStats)
-router.patch('/update-user', validateUpdateUserInput, updateUser)
+router.patch('/update-user', upload.single('avatar'), validateUpdateUserInput, updateUser)
 
 export default router
