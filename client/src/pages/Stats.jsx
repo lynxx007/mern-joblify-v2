0196646614d01e4b95baa1661b98126dc59bb3dc
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-23 12:29:21 +0700

    created Stats page

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { ChartContainer, StatsContainer } from "../components"
+import { useShowStatsQuery } from "../features/api/apiSlice"
 
 const Stats = () => {
-    return (
-        <div>Stats</div>
-    )
+    const { data, isSuccess } = useShowStatsQuery()
+    if (isSuccess)
+        return (
+            <>
+                <StatsContainer defaultStats={data?.defaultStats} />
+                {data?.monthlyStats?.length > 0 && (
+                    <ChartContainer data={data?.monthlyStats} />
+                )}
+            </>
+        )
 }
 
 export default Stats
