f2d924f6bb920456b3e30ea242f5402f356b13ef
lynxx007 <luthfirizky7@gmail.com>
2024-01-25 09:19:59 +0700

    changed public dir to dist dir

--- server.js
+++ server.js
@@ -1,55 +1,44 @@
-import express from 'express'
-import 'express-async-errors'
-import morgan from 'morgan'
-import 'dotenv/config'
-import cookieParser from 'cookie-parser'
-import { notFound } from './middlewares/notFound.js'
-import { errorHandler } from './middlewares/errorHandler.js'
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import { notFound } from "./middlewares/notFound.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
-import jobsRouter from './routers/jobRouter.js'
-import authRouter from './routers/authRouter.js'
-import userRouter from './routers/userRouter.js'
-import { connectDb } from './config/connectDb.js'
-import { authenticateUser } from './middlewares/authMiddleware.js'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import jobsRouter from "./routers/jobRouter.js";
+import authRouter from "./routers/authRouter.js";
+import userRouter from "./routers/userRouter.js";
+import { connectDb } from "./config/connectDb.js";
+import { authenticateUser } from "./middlewares/authMiddleware.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
+await connectDb();
 
+var __dirname = dirname(fileURLToPath(import.meta.url));
 
+const app = express();
 
-await connectDb()
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
-var __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(cookieParser());
 
-const app = express()
-
-app.use(express.static(path.resolve(__dirname, './client/public')))
-
-
-app.use(cookieParser())
-
-
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 
-app.use('/api/v1/jobs', authenticateUser, jobsRouter)
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', authenticateUser, userRouter)
+app.use("*", notFound);
+app.use(errorHandler);
 
-
-
-
-
-app.use('*', notFound)
-app.use(errorHandler)
-
-
-const port = process.env.PORT || 5100
+const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
-    console.log('server is running...')
-})
+  console.log("server is running...");
+});
